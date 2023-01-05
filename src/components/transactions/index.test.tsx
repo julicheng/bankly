9cad445642d03c18a698bc154fea87bb8661585e
Juli Cheng <julich3ng@gmail.com>
2023-01-05 21:18:30 +0000

    test: fix tests

--- src/components/transactions/index.test.tsx
+++ src/components/transactions/index.test.tsx
@@ -1,9 +1,9 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { rest } from 'msw';
 import { TransactionHistory } from ".";
 import { server } from '../../../jest.setup';
 
-describe("transaction history",  () => {
+describe("transaction history", () => {
   test("the expenses tab should be shown by default", async () => {
     render(<TransactionHistory />);
 
@@ -14,21 +14,18 @@ describe("transaction history",  () => {
     });
 
     expect(expensesTabTrigger).toHaveAttribute("data-state", "active");
-    
-    let expensesTable;
-    
-    // Need to wait for the expenses table to appear to then check for its existence
-    await waitFor(() => {
-      expensesTable = screen.getByRole("table", {
-        name: "Expenses",
-      });
-    })
 
-    expect(expensesTable).toBeInTheDocument();
-    expect(screen.getByText("-20.25")).toBeInTheDocument();
+    const expensesTable = screen.queryByRole("table", {
+      name: "Expenses",
+    });
+
+    waitFor(() => {
+      expect(expensesTable).toBeInTheDocument();
+      expect(screen.getByText("-£20.25")).toBeInTheDocument()
+    })
   });
 
-  test.skip("changing between the expenses and income tabs should show different transactions", () => {
+  test("changing between the expenses and income tabs should show different transactions", async () => {
     render(<TransactionHistory />);
 
     const expensesTabTrigger = screen.getByRole("tab", {
@@ -37,23 +34,25 @@ describe("transaction history",  () => {
     const incomeTabTrigger = screen.getByRole("tab", {
       name: "Income",
     });
-    const expensesTable = screen.getByRole("table", {
+    const expensesTable = screen.queryByRole("table", {
       name: "Expenses",
     });
     const incomeTable = screen.queryByRole("table", {
       name: "Income",
     });
 
-    expect(expensesTable).toBeInTheDocument();
-    expect(incomeTable).not.toBeInTheDocument();
+    waitFor(() => {
+      expect(expensesTable).toBeInTheDocument();
+      expect(screen.getByText("-£20.25")).toBeInTheDocument()
+      expect(incomeTable).not.toBeInTheDocument();
 
-    expect(screen.getByText("-20.25")).toBeInTheDocument();
+      fireEvent.click(incomeTabTrigger)
+      expect(incomeTable).toBeInTheDocument()
 
-    incomeTabTrigger.click();
-
-    expect(incomeTabTrigger).toHaveAttribute("data-state", "active");
-    expect(expensesTabTrigger).toHaveAttribute("data-state", "inactive");
-    expect(screen.queryByText("-20.25")).not.toBeInTheDocument();
+      expect(incomeTabTrigger).toHaveAttribute("data-state", "active");
+      expect(expensesTabTrigger).toHaveAttribute("data-state", "inactive");
+      expect(screen.queryByText("-£20.25")).not.toBeInTheDocument();
+    })
   });
 
   test('should show the loading state when transactions have not yet loaded', () => {
@@ -62,7 +61,7 @@ describe("transaction history",  () => {
         res(ctx.delay(2000))
       )
     );
-    
+
     render(<TransactionHistory />);
 
     expect(screen.getByText("Loading...")).toBeInTheDocument();
