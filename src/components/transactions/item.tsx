5416e379498b4892223d30d8f6a152984996aa8a
Juli Cheng <julich3ng@gmail.com>
2023-01-05 20:23:12 +0000

    feat: format dates and prices

--- src/components/transactions/item.tsx
+++ src/components/transactions/item.tsx
@@ -5,22 +5,36 @@ type Props = {
   transaction: TransactionType;
 };
 
-export const Transaction = ({ transaction }: Props) => (
-  <tr>
-    <td>
-      <div className="transaction-detail">
-        <Avatar name={transaction.description} />
-        <div className="transaction-description">
-          {transaction.description}
-          <div className="transaction-category">{transaction.category}</div>
+export const Transaction = ({ transaction }: Props) => {
+  const formattedDate = () => {
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    const d = new Date(transaction.date);
+
+    return `${d.getDate()} ${months[d.getMonth()]} ${d.getUTCFullYear()}`
+  }
+
+  const formattedValue = () => {
+    return new Intl.NumberFormat('en-GB', { style: 'currency', currency: 'GBP' }).format(transaction.amount.value)
+  }
+
+  return (
+    <tr>
+      <td>
+        <div className="transaction-detail">
+          <Avatar name={transaction.description} />
+          <div className="transaction-description">
+            {transaction.description}
+            <div className="transaction-category">{transaction.category}</div>
+          </div>
         </div>
-      </div>
-    </td>
-    <td>
-      <div>{transaction.date}</div>
-    </td>
-    <td className="transaction-amount">
-      <div className="amount">{transaction.amount.value}</div>
-    </td>
-  </tr>
-);
+      </td>
+      <td>
+        <div>{formattedDate()}</div>
+      </td>
+      <td className="transaction-amount">
+        <div className="amount">{formattedValue()}</div>
+      </td>
+    </tr>
+  );
+}
